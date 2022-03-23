cd936ee8a8e6f9ef99b9f0da5d65e17e9d669477
qinzhanbin <102206259+qinzhanbin@users.noreply.github.com>
2022-03-23 13:42:32 +0800

    Update 数字炸弹.py

--- 数字炸弹.py
+++ 数字炸弹.py
@@ -3,11 +3,12 @@ num = random.randint(1,100)
 n = 1
 m = 100
 while True:
-    x = input("说出一个数字：")
+    x = input("输入一个数字\nEnter a number：")
     y = int(x)
 
     if y == num:
-        print("炸弹来咯~")
+        print("炸弹来咯~\nThe bomb to cough up")
+        print("乖乖接受惩罚吧\nJust accept your punishment")
         break
 
     else:
@@ -17,13 +18,13 @@ while True:
                 print('%d~%d'%(y,m))
                 n = y
             else:
-                print('不可以这样哦,请重新说一个数字')
+                print('不可以这样哦,请重新说一个数字\nYou cant do that,Please re-enter')
         else:
             if y < m:
                 print('%d~%d'%(n,y))
                 m = y
             else:
-                print('不可以这样哦,请重新说一个数字')
+                print('不可以这样哦,请重新说一个数字\nYou cant do that,Please re-enter')
 
         #print(n)
         #print(m)
